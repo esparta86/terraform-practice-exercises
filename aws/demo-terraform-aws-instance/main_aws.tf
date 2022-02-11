4464d80cf50542269308f56f670c022ffde5b488
lisandro_rafaelano <lisandro_rafaelano@epam.com>
2022-02-11 10:47:45 -0600

    documented all resources

--- aws/demo-terraform-aws-instance/main_aws.tf
+++ aws/demo-terraform-aws-instance/main_aws.tf
@@ -15,6 +15,9 @@ provider "aws" {
 }
 
 #Create VPC
+# check variablees.tf 
+# @default_tags contains default tags to inject into resources
+# @vpc_tags contains the name of vpc
 resource "aws_vpc" "development_vpc" {
   cidr_block = var.development_vpc_cidr
   instance_tenancy = "default"  
@@ -22,36 +25,49 @@ resource "aws_vpc" "development_vpc" {
 }
 
 
-#Create Internet Gateway and attach VPC
 
+
+#Create Internet Gateway and attach VPC
+# check variablees.tf 
+# @default_tags contains default tags to inject into resources
+# @igw_tags contains the name of Intenet Gateway
 resource "aws_internet_gateway" "IGW_development" {
   vpc_id = aws_vpc.development_vpc.id  # vpc id will take the value of vpc id when is created
   tags = merge(var.default_tags,var.igw_tags)
 }
 
 
-#Create a private Subnet
-
+#Create a public subnet
+# check variablees.tf 
+# @default_tags contains default tags to inject into resources
+# @public_subnet_tags contains the name of public subnet
+# @develop_public_subnet_cidr contains the CIDR that subnet is going to use
 resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.development_vpc.id
-  cidr_block = var.develop_private_subnet_cidr
+  cidr_block = var.develop_public_subnet_cidr
   tags = merge(var.default_tags,var.public_subnet_tags)
 }
 
-#Create a public subnet
-
+#Create a private subnet
+# check variablees.tf 
+# @default_tags contains default tags to inject into resources
+# @private_subnet_tags contains the name of private subnet
+# @develop_private_subnet_cidr contains the CIDR that subnet is going to use
 resource "aws_subnet" "private_subnet" {
   vpc_id = aws_vpc.development_vpc.id
-  cidr_block = var.develop_public_subnet_cidr
+  cidr_block = var.develop_private_subnet_cidr
   tags = merge(var.default_tags,var.private_subnet_tags)
 }
 
-#Route table for public subnets
 
+
+#Route table for public subnets
+# @default_tags contains default tags to inject into resources
 resource "aws_route_table" "publicRT" {
   vpc_id = aws_vpc.development_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
+    # Traffic from public subnets reaches the Internet via Internet Gateway created
     gateway_id = aws_internet_gateway.IGW_development.id
   }
 
@@ -62,11 +78,12 @@ resource "aws_route_table" "publicRT" {
 
 
 #Route table for private subnets
-
+# @default_tags contains default tags to inject into resources
 resource "aws_route_table" "privateRT" {
   vpc_id = aws_vpc.development_vpc.id
   route  {
     cidr_block = "0.0.0.0/0"
+    # Traffic from private subnets reaches the Internet via NAT Gateway.
     nat_gateway_id = aws_nat_gateway.natGW.id
   }
 
@@ -83,16 +100,14 @@ resource "aws_route_table_association" "publicRTassociation" {
 }
 
 #Route table Association with private Subnets
-
 resource "aws_route_table_association" "privateRTassociation" {
   subnet_id = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.privateRT.id
-
 }
 
 
-#Creating Elastic IP addresses
-
+#Creating Elastic IP addresses for NAT Gateway
+# @default_tags contains default tags to inject into resources
 resource "aws_eip" "natIP" {
   vpc = true 
   tags = merge(var.default_tags,{
@@ -101,9 +116,14 @@ resource "aws_eip" "natIP" {
 }
 
 #Creating the NAT gateway using the subnet id and allocation id
-
+# Review the sample https://aws.amazon.com/premiumsupport/knowledge-center/nat-gateway-vpc-private-subnet/
+# IMPORTANT!
+# - To create a NAT gateway is required to have a public subnet that is going to host the NAT gateway
+#   The route table for the PUBLIC subnet should contains a route to the Internet through an Internet gateway
+# - Provision an unattached Elastic IP address (EIP)
+# - You need to update the route table of the private subnet hosting the EC2 instances that need internet access
 resource "aws_nat_gateway" "natGW" {
-  allocation_id = aws_eip.natIP.id
+  allocation_id = aws_eip.natIP.id  #Elastic IP ID
   subnet_id = aws_subnet.public_subnet.id
   
   tags = merge(var.default_tags,{
@@ -116,25 +136,26 @@ resource "aws_nat_gateway" "natGW" {
 }
 
 
-
-
-
+# Creating the first NGINX server on public subnet
+# @instance_ami contains the ami id of UBUNTU linux distribution
+# The EC2 requires a security group, that security group let us 
+#  - Access to the content through 80 port
+#  - Access using ssh through 22 port
+#  - Test a ping command.
+# setup-nginx.sh contains all script bash command to install an nginx server
+# key_name is important if you are going to associate a key to access to the server through ssh
 resource "aws_instance" "app_server" {
   ami           = var.instance_ami
   instance_type = "t2.medium"
 
-  # user_data = <<-EOL
-  # #!/bin/bash -xe
-  # apt-get update && apt-get install -y nginx
-  # echo "NGINX INSTALLED"
   depends_on = [
     aws_security_group.security_group
   ]
+
   vpc_security_group_ids = [ aws_security_group.security_group.id ]
-
-
-  # EOL
+  
   subnet_id = aws_subnet.public_subnet.id
+  
   user_data = "${file("setup-nginx.sh")}"
 
   tags = merge(var.default_tags,{
@@ -142,11 +163,10 @@ resource "aws_instance" "app_server" {
   })
 
   key_name = "ubuntu"
-
 }
 
-#Creating Elastic IP addresses
 
+#Creating Elastic IP addresses  and associate with the web server created
 resource "aws_eip" "publicIPServer01" {
   vpc = true
 
