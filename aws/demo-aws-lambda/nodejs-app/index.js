fe741cd4809804f2f5a19f5ad0f296f55a3c3746
lisandro_rafaelano <lisandro@nursa.com>
2022-08-29 22:10:53 -0600

    feat(tf) : added lambda resource v4

--- aws/demo-aws-lambda/nodejs-app/index.js
+++ aws/demo-aws-lambda/nodejs-app/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Express version 2!');
+  res.send('Hello Express version 4!');
 });
 
 app.listen(port, () => {
